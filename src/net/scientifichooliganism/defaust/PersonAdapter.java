f6bffbddb691d106d9683894848a781c7f432b0a
John Tatum <john.tatum@live.com>
2019-03-28 02:08:54 -0500

    Created PersonAdapter to properly format field inherited from Entity abstract class.

---
+++ src/net/scientifichooliganism/defaust/PersonAdapter.java
@@ -0,0 +1,40 @@
+package net.scientifichooliganism.defaust;
+
+import net.scientifichooliganism.defaust.vo.Person;
+
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializer;
+import com.google.gson.JsonSerializationContext;
+
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+
+public class PersonAdapter implements JsonDeserializer<Person>, JsonSerializer<Person> {
+	@Override
+	public Person deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+		Person person = new Person();
+		JsonObject jsonObject = jsonElement.getAsJsonObject();
+		JsonElement element;
+
+		if((element = jsonObject.get("dateOfBirth")) != null){
+			person.setDateOfBirth(element.getAsString());
+		}
+
+		return person;
+	}
+
+	@Override
+	public JsonElement serialize(Person person, Type type, JsonSerializationContext jsonSerializationContext) {
+		JsonObject jsonObject = new JsonObject();
+
+		if(person.getDateOfBirth() != null){
+			jsonObject.addProperty("dateOfBirth", new SimpleDateFormat("yyyy-MM-dd").format(person.getDateOfBirth()));
+		}
+
+		return jsonObject;
+	}
+}
--- tst/net/scientifichooliganism/defaust/vo/PersonTest.java
+++ tst/net/scientifichooliganism/defaust/vo/PersonTest.java
@@ -1,6 +1,9 @@
 package net.scientifichooliganism.defaust.vo;
 
+import net.scientifichooliganism.defaust.PersonAdapter;
+
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.junit.runner.*;
@@ -14,6 +17,6 @@ public class PersonTest {
 		p.addName("Charles");
 		p.setDateOfBirth("1901-01-01");
 
-		Assert.assertEquals("{\"name\":[{\"id\":0,\"index\":0,\"name\":\"Alfred\"},{\"id\":1,\"index\":1,\"name\":\"Bob\"},{\"id\":2,\"index\":2,\"name\":\"Charles\"}],\"dateOfCreation\":\"1901-01-01",\"summary\":\"\",\"description\":\"\"}", new Gson().toJson(p));
+		Assert.assertEquals("{\"name\":[{\"id\":0,\"index\":0,\"name\":\"Alfred\"},{\"id\":1,\"index\":1,\"name\":\"Bob\"},{\"id\":2,\"index\":2,\"name\":\"Charles\"}],\"dateOfBirth\":\"1901-01-01\",\"summary\":\"\",\"description\":\"\"}", new GsonBuilder().registerTypeAdapter(Person.class, new PersonAdapter()).setDateFormat("yyyy-MM-dd").create().toJson(p));
 	}
 }
